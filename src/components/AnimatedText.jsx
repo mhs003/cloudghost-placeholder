713a5d0cbb453e3573f8378263cc58428a36d622
Monzurul Hasan <49614333+mhs003@users.noreply.github.com>
2023-09-16 19:33:48 +0600

    Update AnimatedText.jsx

--- src/components/AnimatedText.jsx
+++ src/components/AnimatedText.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-// Word wrapper
 const Wrapper = (props) => {
-    // We'll do this to prevent wrapping of words using CSS
     return <span className="word-wrapper">{props.children}</span>;
 };
 
-// AnimatedCharacters
-// Handles the deconstruction of each word and character to setup for the
-// individual character animations
 const AnimatedCharacters = (props) => {
-    // Framer Motion variant object, for controlling animation
     const item = {
         hidden: {
             y: "200%",
@@ -25,18 +19,14 @@ const AnimatedCharacters = (props) => {
         },
     };
 
-    //  Split each word of props.text into an array
     const splitWords = props.text.split(" ");
 
-    // Create storage array
     const words = [];
 
-    // Push each word into words array
     for (const [, item] of splitWords.entries()) {
         words.push(item.split(""));
     }
 
-    // Add a space ("\u00A0") to the end of each word
     words.map((word) => {
         return word.push("\u00A0");
     });
@@ -45,7 +35,6 @@ const AnimatedCharacters = (props) => {
         <div className={props.className}>
             {words.map((word, index) => {
                 return (
-                    // Wrap each word in the Wrapper component
                     <Wrapper key={index}>
                         {words[index].flat().map((element, index) => {
                             return (
@@ -68,7 +57,6 @@ const AnimatedCharacters = (props) => {
                     </Wrapper>
                 );
             })}
-            {/* {} */}
         </div>
     );
 };
